e866bc893d0ddfe4b24b9ebb782d12cd9d8cc555
CreatorSiSo <64036709+CreatorSiSo@users.noreply.github.com>
2021-10-27 00:42:59 +0200

    Add glfw initialisation code.

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,34 @@
+#include <GLFW/glfw3.h>
+
 #include <log.hpp>
 
 int main() {
-    log("test", 0);
-    return 0;
+    GLFWwindow *window;
+
+    if(!glfwInit()) {
+        log("Failed to initialize GLFW", 1);
+        exit(EXIT_FAILURE);
+    }
+
+    window = glfwCreateWindow(900, 600, "template-CppCmake", NULL, NULL);
+    
+    if (!window) {
+        log("Failed to open GLFW window", 1);
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    // Main loop
+    while(!glfwWindowShouldClose(window))
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
+    // Exit program
+    exit(EXIT_SUCCESS);
 }
